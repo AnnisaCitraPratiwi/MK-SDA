d56406bd08417153fc541caa1e2923d208b66b42
Annisa Citra Pratiwi <112589538+AnnisaCitraPratiwi@users.noreply.github.com>
2023-03-25 19:45:59 +0700

    Add files via upload

---
+++ Kalkulator.py
@@ -0,0 +1,29 @@
+
+print('=' * 25)
+print('Operasi Matematika')
+print(' 1. Penjumlahan [+]')
+print(' 2. Pengurangan [-]')
+print(' 3. Perkalian   [*]')
+print(' 4. Pembagian   [/]')
+print('=' * 25)
+
+operasi = input('Pilih operasi (1/2/3/4): ')
+bilangan_1 = eval(input('Masukkan bilangan pertama: '))
+bilangan_2 = eval(input('Masukkan bilangan kedua: '))
+
+if operasi == '1':
+  hasil = bilangan_1 + bilangan_2
+  print(f'Hasil operasi dari {bilangan_1} + {bilangan_2} = {hasil}')
+  return operasi
+  
+elif operasi == '2':
+  hasil = bilangan_1 - bilangan_2
+  print(f'Hasil operasi dari {bilangan_1} - {bilangan_2} = {hasil}')
+elif operasi == '3':
+  hasil = bilangan_1 * bilangan_2
+  print(f'Hasil operasi dari {bilangan_1} * {bilangan_2} = {hasil}')
+elif operasi == '4':
+  hasil = bilangan_1 / bilangan_2
+  print(f'Hasil operasi dari {bilangan_1} / {bilangan_2} = {hasil}')
+else:
+  print('Tidak valid')
